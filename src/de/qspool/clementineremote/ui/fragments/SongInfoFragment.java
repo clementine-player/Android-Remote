759ec27da2678d6129582788d9ea0eb3df700922
Andreas <asfa194@gmail.com>
2013-09-29 23:08:28 +0200

    Zoom image on click in song information fragment.

--- src/de/qspool/clementineremote/ui/fragments/SongInfoFragment.java
+++ src/de/qspool/clementineremote/ui/fragments/SongInfoFragment.java
@@ -18,11 +18,20 @@ his file is part of the Android Clementine Remote.
 
 package de.qspool.clementineremote.ui.fragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -38,7 +47,9 @@ import de.qspool.clementineremote.backend.player.MySong;
 import de.qspool.clementineremote.utils.Utilities;
 
 public class SongInfoFragment extends AbstractDrawerFragment {
-	private ImageView iv_art;
+	private FrameLayout mContainer;
+	private ImageButton iv_art;
+	private ImageView iv_large_art;
 	
 	private TextView tv_artist;
 	private TextView tv_title;
@@ -53,6 +64,17 @@ public class SongInfoFragment extends AbstractDrawerFragment {
 	private TextView tv_filename;
 	
 	private RatingBar rb_rating;
+	
+	// Hold a reference to the current animator,
+    // so that it can be canceled mid-way.
+    private Animator mCurrentAnimator;
+
+    // The system "short" animation time duration, in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // very frequently.
+    private int mShortAnimationDuration;
+
+    private MySong mCurrentSong;
     
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,7 +82,21 @@ public class SongInfoFragment extends AbstractDrawerFragment {
 		View view = inflater.inflate(R.layout.songinfo_fragment,
 				container, false);
 		
-		iv_art = (ImageView) view.findViewById(R.id.si_art);
+		mContainer = (FrameLayout) view.findViewById(R.id.si_container);
+		
+		iv_art = (ImageButton) view.findViewById(R.id.si_art);
+		iv_large_art = (ImageView) view.findViewById(R.id.si_large_art);
+		
+		iv_art.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(iv_art);
+            }
+        });
+
+		// Retrieve and cache the system's default "short" animation time.
+        mShortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
 		
 		tv_artist = (TextView) view.findViewById(R.id.si_artist);
 		tv_title  = (TextView) view.findViewById(R.id.si_title);
@@ -158,5 +194,146 @@ public class SongInfoFragment extends AbstractDrawerFragment {
 	    		iv_art.setImageBitmap(currentSong.getArt());
 	    	}
     	}
+    	
+    	mCurrentSong = currentSong;
     }
+	
+	private void zoomImageFromThumb(final View thumbView) {
+	    // If there's an animation in progress, cancel it
+	    // immediately and proceed with this one.
+	    if (mCurrentAnimator != null) {
+	        mCurrentAnimator.cancel();
+	    }
+	    
+	    // If we don't have an image, do not zoom!
+	    if (mCurrentSong.getArt() == null) 
+	    	return;
+
+	    // Load the high-resolution "zoomed-in" image.
+	    iv_large_art.setImageBitmap(mCurrentSong.getArt());
+
+	    // Calculate the starting and ending bounds for the zoomed-in image.
+	    // This step involves lots of math. Yay, math.
+	    final Rect startBounds = new Rect();
+	    final Rect finalBounds = new Rect();
+	    final Point globalOffset = new Point();
+
+	    // The start bounds are the global visible rectangle of the thumbnail,
+	    // and the final bounds are the global visible rectangle of the container
+	    // view. Also set the container view's offset as the origin for the
+	    // bounds, since that's the origin for the positioning animation
+	    // properties (X, Y).
+	    thumbView.getGlobalVisibleRect(startBounds);
+	    mContainer.getGlobalVisibleRect(finalBounds, globalOffset);
+	    startBounds.offset(-globalOffset.x, -globalOffset.y);
+	    finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+	    // Adjust the start bounds to be the same aspect ratio as the final
+	    // bounds using the "center crop" technique. This prevents undesirable
+	    // stretching during the animation. Also calculate the start scaling
+	    // factor (the end scaling factor is always 1.0).
+	    float startScale;
+	    if ((float) finalBounds.width() / finalBounds.height()
+	            > (float) startBounds.width() / startBounds.height()) {
+	        // Extend start bounds horizontally
+	        startScale = (float) startBounds.height() / finalBounds.height();
+	        float startWidth = startScale * finalBounds.width();
+	        float deltaWidth = (startWidth - startBounds.width()) / 2;
+	        startBounds.left -= deltaWidth;
+	        startBounds.right += deltaWidth;
+	    } else {
+	        // Extend start bounds vertically
+	        startScale = (float) startBounds.width() / finalBounds.width();
+	        float startHeight = startScale * finalBounds.height();
+	        float deltaHeight = (startHeight - startBounds.height()) / 2;
+	        startBounds.top -= deltaHeight;
+	        startBounds.bottom += deltaHeight;
+	    }
+
+	    // Hide the thumbnail and show the zoomed-in view. When the animation
+	    // begins, it will position the zoomed-in view in the place of the
+	    // thumbnail.
+	    thumbView.setAlpha(0f);
+	    iv_large_art.setVisibility(View.VISIBLE);
+
+	    // Set the pivot point for SCALE_X and SCALE_Y transformations
+	    // to the top-left corner of the zoomed-in view (the default
+	    // is the center of the view).
+	    iv_large_art.setPivotX(0f);
+	    iv_large_art.setPivotY(0f);
+
+	    // Construct and run the parallel animation of the four translation and
+	    // scale properties (X, Y, SCALE_X, and SCALE_Y).
+	    AnimatorSet set = new AnimatorSet();
+	    set
+	            .play(ObjectAnimator.ofFloat(iv_large_art, View.X,
+	                    startBounds.left, finalBounds.left))
+	            .with(ObjectAnimator.ofFloat(iv_large_art, View.Y,
+	                    startBounds.top, finalBounds.top))
+	            .with(ObjectAnimator.ofFloat(iv_large_art, View.SCALE_X,
+	            startScale, 1f)).with(ObjectAnimator.ofFloat(iv_large_art,
+	                    View.SCALE_Y, startScale, 1f));
+	    set.setDuration(mShortAnimationDuration);
+	    set.setInterpolator(new DecelerateInterpolator());
+	    set.addListener(new AnimatorListenerAdapter() {
+	        @Override
+	        public void onAnimationEnd(Animator animation) {
+	            mCurrentAnimator = null;
+	        }
+
+	        @Override
+	        public void onAnimationCancel(Animator animation) {
+	            mCurrentAnimator = null;
+	        }
+	    });
+	    set.start();
+	    mCurrentAnimator = set;
+
+	    // Upon clicking the zoomed-in image, it should zoom back down
+	    // to the original bounds and show the thumbnail instead of
+	    // the expanded image.
+	    final float startScaleFinal = startScale;
+	    iv_large_art.setOnClickListener(new View.OnClickListener() {
+	        @Override
+	        public void onClick(View view) {
+	            if (mCurrentAnimator != null) {
+	                mCurrentAnimator.cancel();
+	            }
+
+	            // Animate the four positioning/sizing properties in parallel,
+	            // back to their original values.
+	            AnimatorSet set = new AnimatorSet();
+	            set.play(ObjectAnimator
+	                        .ofFloat(iv_large_art, View.X, startBounds.left))
+	                        .with(ObjectAnimator
+	                                .ofFloat(iv_large_art, 
+	                                        View.Y,startBounds.top))
+	                        .with(ObjectAnimator
+	                                .ofFloat(iv_large_art, 
+	                                        View.SCALE_X, startScaleFinal))
+	                        .with(ObjectAnimator
+	                                .ofFloat(iv_large_art, 
+	                                        View.SCALE_Y, startScaleFinal));
+	            set.setDuration(mShortAnimationDuration);
+	            set.setInterpolator(new DecelerateInterpolator());
+	            set.addListener(new AnimatorListenerAdapter() {
+	                @Override
+	                public void onAnimationEnd(Animator animation) {
+	                    thumbView.setAlpha(1f);
+	                    iv_large_art.setVisibility(View.GONE);
+	                    mCurrentAnimator = null;
+	                }
+
+	                @Override
+	                public void onAnimationCancel(Animator animation) {
+	                    thumbView.setAlpha(1f);
+	                    iv_large_art.setVisibility(View.GONE);
+	                    mCurrentAnimator = null;
+	                }
+	            });
+	            set.start();
+	            mCurrentAnimator = set;
+	        }
+	    });
+	}
 }
