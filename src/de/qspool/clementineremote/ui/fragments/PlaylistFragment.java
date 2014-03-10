d4d1c112e0a5309c8905ded8fca2354470186802
Andreas <asfa194@gmail.com>
2014-03-10 22:19:50 +0100

    Make sure fast scrolling is enabled when switching playlists.
    More information: https://stackoverflow.com/questions/22287614

--- src/de/qspool/clementineremote/ui/fragments/PlaylistFragment.java
+++ src/de/qspool/clementineremote/ui/fragments/PlaylistFragment.java
@@ -323,6 +323,18 @@ public class PlaylistFragment extends AbstractDrawerFragment {
                 getSelectedPlaylistSongs());
         mList.setAdapter(mAdapter);
 
+        // We have to post notifyDataSetChanged() here, so fast scroll is set correctly.
+        // Without it, the fast scroll cannot get any child views as the adapter is not yet fully
+        // attached to the view and getChildCount() returns 0. Therefore, fast scroll won't
+        // be enabled.
+        // notifyDataSetChanged() forces the listview to recheck the fast scroll preconditions.
+        mList.post(new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
         updateViewPosition();
 
         if (mPlaylists.isEmpty()) {
