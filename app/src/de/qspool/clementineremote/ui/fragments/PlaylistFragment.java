d211adb33d29bab5daead97a5491d64924df68c6
Andreas <asfa194@gmail.com>
2014-09-25 20:51:59 +0200

    Delete multiple items from a playlist. Updates #76.

--- app/src/de/qspool/clementineremote/ui/fragments/PlaylistFragment.java
+++ app/src/de/qspool/clementineremote/ui/fragments/PlaylistFragment.java
@@ -24,6 +24,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
+import android.util.SparseBooleanArray;
 import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -33,6 +34,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
@@ -52,6 +54,7 @@ import de.qspool.clementineremote.backend.event.OnPlaylistReceivedListener;
 import de.qspool.clementineremote.backend.pb.ClementineMessage;
 import de.qspool.clementineremote.backend.pb.ClementineMessageFactory;
 import de.qspool.clementineremote.backend.pb.ClementineRemoteProtocolBuffer.DownloadItem;
+import de.qspool.clementineremote.backend.player.MyLibraryItem;
 import de.qspool.clementineremote.backend.player.MyPlaylist;
 import de.qspool.clementineremote.backend.player.MySong;
 import de.qspool.clementineremote.backend.player.PlaylistManager;
@@ -65,17 +68,9 @@ public class PlaylistFragment extends AbstractDrawerFragment {
 
     private ActionBar mActionBar;
 
-    private ActionMode mActionMode;
-
     private ListView mList;
 
     private View mEmptyPlaylist;
-
-    private View mSelectedItem;
-
-    private Drawable mSelectedItemDrawable;
-
-    private MySong mSelectedSong;
 
     private PlaylistManager mPlaylistManager;
 
@@ -207,26 +202,68 @@ public class PlaylistFragment extends AbstractDrawerFragment {
 
         mList.setOnItemClickListener(oiclSong);
         mList.setAdapter(mAdapter);
-        mList.setLongClickable(true);
-        mList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+        mList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mList.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public boolean onActionItemClicked(ActionMode mode,
+                    android.view.MenuItem item) {
+                SparseBooleanArray checkedPositions = mList.getCheckedItemPositions();
+
+                switch (item.getItemId()) {
+                    case R.id.playlist_context_play:
+                        for (int i = 0; i < checkedPositions.size(); ++i) {
+                            int position = checkedPositions.keyAt(i);
+                            if (checkedPositions.valueAt(i)) {
+                                MySong s = getSelectedPlaylistSongs().get(position);
+                                playSong(s);
+                                break;
+                            }
+                        }
+                        mode.finish();
+                        return true;
+                    case R.id.playlist_context_remove:
+                        LinkedList<MySong> songs = new LinkedList<>();
+                        for (int i = 0; i < checkedPositions.size(); ++i) {
+                            int position = checkedPositions.keyAt(i);
+                            if (checkedPositions.valueAt(i)) {
+                                MySong s = getSelectedPlaylistSongs().get(position);
+                                songs.add(s);
+                            }
+                        }
+                        Message msg = Message.obtain();
+                        msg.obj = ClementineMessageFactory
+                                .buildRemoveMultipleSongsFromPlaylist(getPlaylistId(),
+                                        songs);
+                        App.mClementineConnection.mHandler.sendMessage(msg);
+                        mode.finish();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
 
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view,
-                    int position, long id) {
-                if (mActionMode != null) {
-                    return false;
-                }
-
-                mSelectedItem = view;
-                mSelectedItemDrawable = mSelectedItem.getBackground();
-                mSelectedItem.setBackgroundColor(getResources().getColor(R.color.orange_light));
-
-                mSelectedSong = getSelectedPlaylistSongs().get(position);
-
-                // Start the CAB using the ActionMode.Callback defined above
-                mActionMode = getActivity().startActionMode(mActionModeCallback);
-                view.setSelected(true);
+            public boolean onCreateActionMode(ActionMode mode,
+                    android.view.Menu menu) {
+                android.view.MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.playlist_context_menu, menu);
                 return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode,
+                    android.view.Menu menu) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+            }
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode,
+                    int position, long id, boolean checked) {
             }
         });
 
@@ -396,7 +433,8 @@ public class PlaylistFragment extends AbstractDrawerFragment {
      * Set the selection to the currently played item
      */
     private void updateViewPosition() {
-        if (App.mClementine.getCurrentSong() != null) {
+        if (App.mClementine.getCurrentSong() != null
+                && mPlaylistManager.getActivePlaylistId() == getPlaylistId()) {
             int pos = App.mClementine.getCurrentSong().getIndex();
             mList.setSelection(pos - mSelectionOffset);
         }
@@ -483,58 +521,4 @@ public class PlaylistFragment extends AbstractDrawerFragment {
         }
         return mPlaylists.get(pos).getPlaylistSongs();
     }
-
-    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
-
-        // Called when the action mode is created; startActionMode() was called
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            // Inflate a menu resource providing context menu items
-            MenuInflater inflater = mode.getMenuInflater();
-            inflater.inflate(R.menu.playlist_context_menu, menu);
-            return true;
-        }
-
-        // Called each time the action mode is shown. Always called after onCreateActionMode, but
-        // may be called multiple times if the mode is invalidated.
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return false; // Return false if nothing is done
-        }
-
-        // Called when the user selects a contextual menu item
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.playlist_context_play:
-                    playSong(mSelectedSong);
-                    mode.finish();
-                    return true;
-                case R.id.playlist_context_remove:
-                    Message msg = Message.obtain();
-                    msg.obj = ClementineMessageFactory
-                            .buildRemoveSongFromPlaylist(getPlaylistId(), mSelectedSong);
-                    App.mClementineConnection.mHandler.sendMessage(msg);
-                    mAdapter.remove(mSelectedSong);
-                    mAdapter.notifyDataSetChanged();
-                    mode.finish();
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        // Called when the user exits the action mode
-        @SuppressWarnings("deprecation")
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            mActionMode = null;
-            mSelectedSong = null;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                mSelectedItem.setBackground(mSelectedItemDrawable);
-            } else {
-                mSelectedItem.setBackgroundDrawable(mSelectedItemDrawable);
-            }
-        }
-    };
 }
